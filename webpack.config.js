531154f0680b6f2dceef647d3f8059dd6cf5b15a
John Morton <john@johnfmorton.com>
2017-08-16 17:42:24 -0400

    Added ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -3,61 +3,72 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 const PATHS = {
-    app: path.join(__dirname, 'app'),
-    build: path.join(__dirname, 'build'),
-}
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
 
 const commonConfig = {
-    entry: {
-        app: PATHS.app,
-    },
-    output: {
-        path: PATHS.build,
-        filename: '[name].js',
-    },
-    plugins: [
-    	new FriendlyErrorsWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Webpack Demo!',
-        }),
-    ],
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js',
+  },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Demo!',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        },
+      }],
+  },
 };
 
 const productionConfig = () => commonConfig;
 
 const developmentConfig = () => {
-    const config = {
-        devServer: {
-            // Enable history API fallback so HTML5 History API based
-            // routing works. Good for complex setups.
-            historyApiFallback: true,
+  const config = {
+    devServer: {
+      // Enable history API fallback so HTML5 History API based
+      // routing works. Good for complex setups.
+      historyApiFallback: true,
 
-            // Display only errors to reduce the amount of output.
-            stats: 'errors-only',
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
 
-            // Parse host and port from env to allow customization.
-            //
-            // If you use Docker, Vagrant or Cloud9, set
-            // host: options.host || '0.0.0.0';
-            //
-            // 0.0.0.0 is available to all network devices
-            // unlike default `localhost`.
-            host: process.env.HOST, // Defaults to `localhost`
-            port: process.env.PORT, // Defaults to 8080
-        },
-    };
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Docker, Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+    },
+  };
 
-    return Object.assign({},
-        commonConfig,
-        config
-    );
+  return Object.assign({},
+    commonConfig,
+    config
+  );
 };
 
 module.exports = (env) => {
-    if (env === 'production') {
-        return productionConfig();
-    }
+  if (env === 'production') {
+    return productionConfig();
+  }
 
-    return developmentConfig();
+  return developmentConfig();
 
 };
